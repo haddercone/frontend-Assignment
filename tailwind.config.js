a3d81f0a1734b003d42bfcc5786cc31cc6b9757a
haddercone <44robin.rs@gmail.com>
2023-07-14 09:46:16 +0530

    tailwind configuration added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "hero-section" : "linear-gradient(to right , white, transparent), url(https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80)",
+      }
+    },
+  },
+  plugins: [],
+}
+
